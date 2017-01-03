46328845b42f68bd22c30a1cfb4c0d4afee26509
EduardoTel <eduardo_jonathan@outlook.com>
2017-01-03 13:43:19 -0600

    addListener

--- main_script.js
+++ main_script.js
@@ -3,9 +3,10 @@ function openInNewTab(url) {
   win.focus();
 }
 function search_click(info,tab){
-    openInNewTab(info);
-    var searchstring = info.selectionText;
-    chrome.tabs.create({url: "https://duckduckgo.com/?q=%21translate+"+searchstring});
+    //openInNewTab(info);
+    //var searchstring = info.selectionText;
+    //alert(JSON.stringify(info,null,"\t"));
+    chrome.tabs.create({url: info});
     //alert(info);
     //console.log("__________________________0");
     //console.log(JSON.stringify(info, null, '\t'));
@@ -13,47 +14,46 @@ function search_click(info,tab){
 }
 //chrome.runtime.onInstalled.addListener(function() {
     //_________________________________________________________________________Busquedas
-    var Busquedas = chrome.contextMenus.create({"title": "Busquedas"});
-    var Busquedas1 = chrome.contextMenus.create({"id": "Busquedas_youtube","title": "Buscar en YouTube", "parentId": Busquedas, "onclick": search_click("https://duckduckgo.com/?q=%21translate+car")});
-    var Busquedas2 = chrome.contextMenus.create({"id": "Busquedas_wikipedia","title": "Buscar wikipedia", "parentId": Busquedas, "onclick": search_click("https://duckduckgo.com/?q=%21wes+carros&t=h_")});
+    var Busquedas  = chrome.contextMenus.create({"title": "Busquedas"});
+    var Busquedas1 = chrome.contextMenus.create({"id"   : "Busquedas_youtube","title": "Buscar en YouTube", "parentId": Busquedas});
+    var Busquedas2 = chrome.contextMenus.create({"id"   : "Busquedas_wikipedia","title": "Buscar wikipedia", "parentId": Busquedas});
     //_________________________________________________________________________Multimedia
-    var multimadia = chrome.contextMenus.create({"title": "Multimedia"});
-    var multimadia1 = chrome.contextMenus.create({"id": "multimadia_googleImagenes","title": "Imagenes en Google", "parentId": multimadia, "onclick": search_click("https://duckduckgo.com/?q=%21pictures+cars")});
+    var multimadia  = chrome.contextMenus.create({"title": "Multimedia"});
+    var multimadia1 = chrome.contextMenus.create({"id"   : "Multimedia_googleImagenes","title": "Imagenes en Google", "parentId": multimadia});
     //_________________________________________________________________________Herramientas
-    var Herramientas = chrome.contextMenus.create({"title": "Herramientas"});
-    var Herramientas1 = chrome.contextMenus.create({"id": "Herramientas_duckduckgoHash","title": "Buscar Hash", "parentId": Herramientas, "onclick": search_click("https://duckduckgo.com/?q=hash+4d186321c1a7f0f354b297e8914ab240&t=h_&ia=answer")});
-    var Herramientas2 = chrome.contextMenus.create({"id": "Herramientas_googleTraducir","title": "Traducir", "parentId": Herramientas, "onclick": search_click("https://duckduckgo.com/?q=%21translate+car")});
+    var Herramientas  = chrome.contextMenus.create({"title": "Herramientas"});
+    var Herramientas1 = chrome.contextMenus.create({"id"   : "Herramientas_duckduckgoHash","title": "Buscar Hash", "parentId": Herramientas});
+    var Herramientas2 = chrome.contextMenus.create({"id"   : "Herramientas_googleTraducir","title": "Traducir", "parentId": Herramientas});
     //_________________________________________________________________________Capturar eventos de todos los contextMenus
     chrome.contextMenus.onClicked.addListener(function(info, tab) {
-        alert(JSON.stringify(info,null,"\t"));
         menuItemId=info.menuItemId;
-        prefijo=menuItemId;
+        prefijo=menuItemId.split('_',1);
+        //alert(JSON.stringify(info,null,"\t"));
+        //alert(JSON.stringify(prefijo,null,"\t"));
         if(prefijo=='Busquedas'){
-             if (menuItemId == "Busquedas_youtube") {
-
+            if (menuItemId == "Busquedas_youtube") {
+                 search_click("https://duckduckgo.com/?q=%21translate+car");
             }else if(menuItemId == "Busquedas_wikipedia"){
-
+                search_click("https://duckduckgo.com/?q=%21wes+carros&t=h");
             }else if(menuItemId == "Busquedas_wikipedia"){
-
+                search_click("https://duckduckgo.com/?q=%21translate+car");
             }
         }else if(prefijo=='Multimedia'){
-             if (menuItemId == "multimadia_googleImagenes") {
-
+            if (menuItemId == "multimadia_googleImagenes") {
+                 search_click("https://duckduckgo.com/?q=%21pictures+cars");
             }else if(menuItemId == "Busquedas_wikipedia"){
-
+                search_click("https://duckduckgo.com/?q=%21translate+car");
             }else if(menuItemId == "Busquedas_wikipedia"){
-
+                search_click("https://duckduckgo.com/?q=%21translate+car");
             }
         }else if(prefijo=='Herramientas'){
             if (menuItemId == "Herramientas_duckduckgoHash") {
-
+                search_click("https://duckduckgo.com/?q=hash+4d186321c1a7f0f354b297e8914ab240&t=h_&ia=answer");
             }else if(menuItemId == "Herramientas_googleTraducir"){
-
+                search_click("https://duckduckgo.com/?q=%21translate+car");
             }else if(menuItemId == "Busquedas_wikipedia"){
-
+                search_click("https://duckduckgo.com/?q=%21translate+car");
             }
         }
-       
-        
     });
 //});
