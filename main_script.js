0ee52f528305ee98a602bb030de0fcc99890a9b2
EduardoTel <eduardo_jonathan@outlook.com>
2017-01-03 16:27:32 -0600

    googleImagenes

--- main_script.js
+++ main_script.js
@@ -16,15 +16,17 @@ function search_click(info,tab){
 //chrome.runtime.onInstalled.addListener(function() {
     //_________________________________________________________________________Busquedas
     var Busquedas  = chrome.contextMenus.create({"title": "Busquedas",contexts:["selection"]});
-    var Busquedas1 = chrome.contextMenus.create({"id"   : "Busquedas_youtube","title": "Buscar en YouTube", "parentId": Busquedas,contexts:["selection"]/*,"onclick" : clickHandler*/});
-    var Busquedas2 = chrome.contextMenus.create({"id"   : "Busquedas_wikipedia","title": "Buscar wikipedia", "parentId": Busquedas,contexts:["selection"]});
+    var Busquedas1 = chrome.contextMenus.create({"id"   : "Busquedas_wikipedia","title": "wikipedia", "parentId": Busquedas,contexts:["selection"]});
+    var Busquedas2 = chrome.contextMenus.create({"id"   : "Busquedas_duckduckgoNationalgeograp","title": "National Geographic", "parentId": Busquedas,contexts:["selection"]});
     //_________________________________________________________________________Multimedia
     var multimadia  = chrome.contextMenus.create({"title": "Multimedia",contexts:["selection"]});
-    var multimadia1 = chrome.contextMenus.create({"id"   : "Multimedia_googleImagenes","title": "Imagenes en Google", "parentId": multimadia,contexts:["selection"]});
+    var multimadia1 = chrome.contextMenus.create({"id"   : "Multimedia_youtube","title": "YouTube", "parentId": multimadia,contexts:["selection"]/*,"onclick" : clickHandler*/});
+    var multimadia2 = chrome.contextMenus.create({"id"   : "Multimedia_googleImagenes","title": "Imagenes en Google", "parentId": multimadia,contexts:["selection"]});
     //_________________________________________________________________________Herramientas
     var Herramientas  = chrome.contextMenus.create({"title": "Herramientas",contexts:["selection"]});
     var Herramientas1 = chrome.contextMenus.create({"id"   : "Herramientas_duckduckgoHash","title": "Buscar Hash", "parentId": Herramientas,contexts:["selection"]});
     var Herramientas2 = chrome.contextMenus.create({"id"   : "Herramientas_googleTraducir","title": "Traducir", "parentId": Herramientas,contexts:["selection"]});
+    var Herramientas3 = chrome.contextMenus.create({"id"   : "Herramientas_googleMaps","title": "Google Maps", "parentId": Herramientas,contexts:["selection"]});
     //_________________________________________________________________________Capturar eventos de todos los contextMenus
     chrome.contextMenus.onClicked.addListener(function(info, tab) {
         menuItemId=info.menuItemId;
@@ -32,27 +34,29 @@ function search_click(info,tab){
         //alert(JSON.stringify(info,null,"\t"));
         //alert(JSON.stringify(prefijo,null,"\t"));
         if(prefijo=='Busquedas'){
-            if (menuItemId == "Busquedas_youtube") {
-                 search_click("https://duckduckgo.com/?q=%21yt+"+info.selectionText+"&t=h_");
-            }else if(menuItemId == "Busquedas_wikipedia"){
+             if(menuItemId == "Busquedas_wikipedia"){
                 search_click("https://duckduckgo.com/?q=%21wes+"+info.selectionText+"&t=h");
             }else if(menuItemId == "Busquedas_wikipedia"){
                 search_click("https://duckduckgo.com/?q=%21translate+"+info.selectionText);
+            }else if(menuItemId == "Busquedas_duckduckgoNationalgeograp"){
+                search_click("https://duckduckgo.com/?q=%21natgeo+"+info.selectionText);
             }
         }else if(prefijo=='Multimedia'){
             if (menuItemId == "multimadia_googleImagenes") {
                  search_click("https://duckduckgo.com/?q=%21pictures+"+info.selectionText);
-            }else if(menuItemId == "Busquedas_wikipedia"){
-                search_click("https://duckduckgo.com/?q=%21translate+"+info.selectionText);
-            }else if(menuItemId == "Busquedas_wikipedia"){
+            }else if(menuItemId == "Multimedia_youtube") {
+                 search_click("https://duckduckgo.com/?q=%21yt+"+info.selectionText+"&t=h_");
+            }else if(menuItemId == "Multimedia_googleImagenes"){
+                search_click("https://duckduckgo.com/?q=%21images+"+info.selectionText+"&t=h_");
+            }else if(menuItemId == "Multimedia_"){
                 search_click("https://duckduckgo.com/?q=%21translate+"+info.selectionText);
             }
         }else if(prefijo=='Herramientas'){
             if (menuItemId == "Herramientas_duckduckgoHash") {
                 search_click("https://duckduckgo.com/?q=hash+"+info.selectionText+"&t=h_&ia=answer");
-            }else if(menuItemId == "Herramientas_googleTraducir"){
-                search_click("https://duckduckgo.com/?q=%21translate+"+info.selectionText);
-            }else if(menuItemId == "Busquedas_wikipedia"){
+            }else if(menuItemId == "Herramientas_googleMaps"){
+                search_click("https://duckduckgo.com/?q=%21gmuk+"+info.selectionText);
+            }else if(menuItemId == "Herramientas_"){
                 search_click("https://duckduckgo.com/?q=%21translate+"+info.selectionText);
             }
         }
