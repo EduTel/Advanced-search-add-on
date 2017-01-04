c876be08a3c1711d79534dd395db26b544dbee80
EduardoTel <eduardo_jonathan@outlook.com>
2017-01-04 16:34:21 -0600

    json_menus 1

--- main_script.js
+++ main_script.js
@@ -43,9 +43,16 @@ function search_click(info,tab){
 for(categoria in json_menus["idiomas"]["es"]){
   console.log(categoria);
   let contador_menu1=chrome.contextMenus.create({"title": categoria,contexts:["selection"]});
-  for(seccion in json_menus["idiomas"]["es"][categoria]){
-    let contador_menu2=chrome.contextMenus.create({"id" :contador_menu1+seccion,"title": json_menus["idiomas"]["es"][categoria][seccion], "parentId": contador_menu1,contexts:["selection"]});
-    console.log(seccion);
+  for(seccion1 in json_menus["idiomas"]["es"][categoria]){
+    if( typeof json_menus["idiomas"]["es"][categoria][seccion1] === 'object' ){
+        let contador_menu2=chrome.contextMenus.create({"id" :contador_menu1+seccion1,"title": seccion1, "parentId": contador_menu1,contexts:["selection"]});
+        for(seccion2 in json_menus["idiomas"]["es"][categoria][seccion1]){
+            let contador_menu3=chrome.contextMenus.create({"id" :contador_menu2+seccion2,"title": json_menus["idiomas"]["es"][categoria][seccion1][seccion2], "parentId": contador_menu1,contexts:["selection"]});
+        }
+    }else{
+        let contador_menu2=chrome.contextMenus.create({"id" :contador_menu1+seccion1,"title": json_menus["idiomas"]["es"][categoria][seccion1], "parentId": contador_menu1,contexts:["selection"]});
+        console.log(seccion1);
+    }
   }
 }
 //chrome.runtime.onInstalled.addListener(function() {
