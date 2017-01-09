5131bf32554ec935efdb04bc1cb41664eef1f7e5
EduardoTellez <eduardo_jonathan@outlook.com>
2017-01-08 18:37:15 -0600

    dropdownmultiple

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
  $('.dropdown-toggle').dropdown();
 document.addEventListener("DOMContentLoaded", function(event) {
+     $('.dropdown-submenu a.test').on("click", function(e){
+            $(this).next('ul').toggle();
+            e.stopPropagation();
+            e.preventDefault();
+     });
      document.querySelector('#search').addEventListener("submit", function(event){
         event.preventDefault();
         info="carros";
         tab="";
-        categoria="Busquedas";
+        categoria="Busquedas";WW
         seccion="Busquedas_wikipedia";
         chrome.runtime.sendMessage({funcion: "execute",parametros:[info,tab,categoria,seccion]},function(response) {
           
