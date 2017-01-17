b441c984541abbf737b09e6f54a3aa0385d30cdf
EduardoTel <eduardo_jonathan@outlook.com>
2017-01-17 12:05:54 -0600

    selection

--- main_script.js
+++ main_script.js
@@ -73,7 +73,7 @@ for(categoria in json_menus["idiomas"][idioma_get]){
   for(seccion1 in json_menus["idiomas"][idioma_get][categoria]){
     if( typeof json_menus["idiomas"][idioma_get][categoria][seccion1] === 'object' ){
         if(Object.keys(json_menus["idiomas"][idioma_get][categoria][seccion1])[0].toString()=="function"){
-            contador_menu2=chrome.contextMenus.create({"id" :Object.values(json_menus["idiomas"][idioma_get][categoria][seccion1])[0].toString(),"title": seccion1, "parentId": contador_menu1,type: "radio",contexts:["selection"]});
+            contador_menu2=chrome.contextMenus.create({"id" :Object.values(json_menus["idiomas"][idioma_get][categoria][seccion1])[0].toString(),"title": seccion1, "parentId": contador_menu1,contexts:["selection"]});
         }else{
             contador_menu2=chrome.contextMenus.create({"id" :seccion1,"title": seccion1, "parentId": contador_menu1,contexts:["selection"]});
         }
@@ -83,9 +83,9 @@ for(categoria in json_menus["idiomas"][idioma_get]){
             if( typeof json_menus["idiomas"][idioma_get][categoria][seccion1][seccion2] === 'object' ){
                 console.log("____________________________entro IF");
                  if(Object.keys(json_menus["idiomas"][idioma_get][categoria][seccion1][seccion2])[0].toString()=="function"){
-                    contador_menu4=chrome.contextMenus.create({"id" :Object.values(json_menus["idiomas"][idioma_get][categoria][seccion1][seccion2])[0].toString(),"title": seccion2, "parentId": contador_menu2,type: "radio",contexts:["selection"]});
+                    contador_menu4=chrome.contextMenus.create({"id" :Object.values(json_menus["idiomas"][idioma_get][categoria][seccion1][seccion2])[0].toString(),"title": seccion2, "parentId": contador_menu2,contexts:["selection"]});
                  }else{
-                    contador_menu2=chrome.contextMenus.create({"id" :seccion1,"title": seccion1, "parentId": contador_menu1,type: "radio",contexts:["selection"]});
+                    contador_menu2=chrome.contextMenus.create({"id" :seccion1,"title": seccion1, "parentId": contador_menu1,contexts:["selection"]});
                  }
             }else{}
         }
